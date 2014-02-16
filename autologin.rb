4b895d51e731c4b36f4e01e47b8c4e10f99d9def
Theldoria <theldoria@hotmail.com>
2014-02-16 17:14:35 +0100

    First commit

---
+++ autologin.rb
@@ -0,0 +1,33 @@
+require 'capybara'
+require 'yaml'
+
+class Autologin
+  def initialize
+    unless File.exist?('config.yml')
+      puts "Create config.yml with:\n#{{user: "your user name", password: "your password"}.to_yaml}"
+      raise "Missing config.yml"
+    end
+
+    @config = YAML.load_file('config.yml')
+
+    @session = Capybara::Session.new(:selenium)
+  end
+
+  def login
+    @session.visit("http://account.dyn.com/entrance")
+    loginbox = @session.find("#loginbox")
+    loginbox.fill_in("username", with: @config[:user])
+    loginbox.fill_in("password", with: @config[:password])
+    loginbox.click_button("Log in")
+  end
+
+  def logout
+    @session.find("#topnav").click_link("Log Out")
+  end
+end
+
+al = Autologin.new
+al.login
+sleep(1)
+al.logout
+sleep(1)
